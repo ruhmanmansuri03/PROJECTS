ea072a2d365965a25bd58e613eee00c5f7bed575
Mansuri Ruhman <162088959+ruhmanmansuri03@users.noreply.github.com>
2024-05-17 11:57:27 +0530

    Update Jumper.java

--- snakeLadder/jumper/Jumper.java
+++ snakeLadder/jumper/Jumper.java
@@ -1,1 +1,21 @@
+package jumper;
 
+import coordinates.Coordinates;
+
+public class Jumper {
+    public Coordinates start;
+    public Coordinates end;
+    public String name;
+
+    public Jumper(Coordinates start, Coordinates end) {
+        this.start = start;
+        this.end = end;
+
+        if (start.getRow() > end.getRow()) this.name = "Ladder";
+        else this.name = "Snake";
+    }
+
+    public String getJumperName() {
+        return this.name;
+    }
+}
